20be2d4f4dde2db7b18befb7f9cfeb52bca048bd
drainerr <george.kvrivi@gmail.com>
2022-07-21 02:52:43 +0400

    change favicon

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -10,7 +10,6 @@ const Main = () => {
 
 	const sendEmail = (e) => {
 		e.preventDefault();
-		console.log(currentUser.displayName, currentUser.email);
 		emailjs.sendForm(
 			process.env.REACT_APP_EMAILJS_SERVICE_ID,
 			process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
